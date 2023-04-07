9b3f2fcd9053874701530088fec9ab2bfe3eb37e
Erney09 <wpuetate@sier.com.co>
2023-04-06 23:58:45 -0500

    creamos una tabla de sqlite

--- prueba2.py
+++ prueba2.py
@@ -1,3 +1,22 @@
-print("esta es mi segunda prueba")
-print("this is a new proff")
-print("hello world")
+import sqlite3
+
+conn= sqlite3.connect("tabla_estudiantes.db")
+cursor= conn.cursor()
+
+cursor.execute("""CREATE TABLE estudiantes(
+            nombre TEXT,
+            edad INTEGER,
+            estatura REAL
+    )""")
+
+cursor.execute("""INSERT INTO estudiantes VALUES("mark", 27, 1.9)""")
+
+list_estudiantes=[
+    ("jhon", 21, 1.8),
+    ("david", 35, 1.7),
+    ("michael", 19, 1.73)
+    ]
+cursor.executemany("INSERT INTO estudiantes VALUES(?, ?, ?)", list_estudiantes)
+
+conn.commit()
+conn.closes
